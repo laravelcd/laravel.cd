456a29147b4f830adbd7ced694763f91b27566e1
Jean Claude Mbiya <mbiyajeanclaude@gmail.com>
2024-12-30 14:58:06 +0100

    logo added

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -5,17 +5,6 @@
             <div class="py-12 sm:py-16 lg:grid lg:grid-cols-3 lg:gap-8 lg:py-20">
                 <div class="space-y-3">
                     <x-brand class="h-12 w-auto text-white sm:h-16" aria-hidden="true" />
-                    <p class="inline-flex flex-wrap items-center text-gray-400 text-sm">
-                        <a href="https://github.com/caneco/laravel-country-logomarks" target="_blank" class="font-medium text-gray-300 underline">
-                            Laravel Country Logomarks
-                        </a>
-                        <span class="ml-1.5">{{ __('global.by') }} Caneco</span>
-                        <img
-                            class="ml-2 size-6 rounded-full"
-                            src="https://avatars.githubusercontent.com/u/502041"
-                            alt="Caneco avatar"
-                        />
-                    </p>
                 </div>
                 <div class="mt-12 grid grid-cols-2 gap-8 lg:mt-0">
                     <div>
@@ -25,7 +14,7 @@
                         <ul class="mt-6 space-y-3">
                             <x-footer-link :title="__('global.navigation.about')" :url="route('about')" />
                             <x-footer-link :title="__('global.navigation.sponsors')" :url="route('sponsors')" />
-                            <x-footer-link :title="__('global.navigation.snippets')" url="https://snippets.laravel.cm" />
+                            <x-footer-link :title="__('global.navigation.snippets')" url="https://snippets.laravel.cd" />
                             <x-footer-link
                                 :title="__('global.navigation.branding')"
                                 url="https://github.com/caneco/laravel-country-logomarks/blob/main/src/cm/README.md"
@@ -54,11 +43,11 @@
                     </div>
                     <div class="mt-6 space-y-6">
                         <div class="flex items-center text-sm font-medium space-x-4 sm:space-x-6">
-                            <a href="https://discord.gg/KNp6brbyVD?utm_source=laravel.cm" target="_blank" class="inline-flex items-center gap-2 text-gray-300 hover:text-white">
+                            <a href="https://discord.gg/KNp6brbyVD?utm_source=laravel.cd" target="_blank" class="inline-flex items-center gap-2 text-gray-300 hover:text-white">
                                 <x-icon.discord class="size-5 text-[#5865F2]" aria-hidden="true" />
                                 Discord
                             </a>
-                            <a href="https://t.me/laravelcameroun?utm_source=laravel.cm" target="_blank" class="inline-flex items-center gap-2 text-gray-300 hover:text-white">
+                            <a href="https://t.me/laravelcameroun?utm_source=laravel.cd" target="_blank" class="inline-flex items-center gap-2 text-gray-300 hover:text-white">
                                 <x-icon.telegram class="size-5 text-[#34AADF]" aria-hidden="true" />
                                 Telegram
                             </a>
@@ -77,22 +66,22 @@
                         <x-icon.twitter class="size-5" aria-hidden="true" />
                     </a>
 
-                    <a href="https://facebook.com/laravelcd?utm_source=laravel.cm" target="_blank" class="text-gray-400 hover:text-gray-300">
+                    <a href="https://facebook.com/laravelcd?utm_source=laravel.cd" target="_blank" class="text-gray-400 hover:text-gray-300">
                         <span class="sr-only">Facebook</span>
                         <x-icon.facebook class="size-6" aria-hidden="true" />
                     </a>
 
-                    <a href="https://www.linkedin.com/company/laravel-cameroun?utm_source=laravel.cm" target="_blank" class="text-gray-400 hover:text-gray-300">
+                    <a href="https://www.linkedin.com/company/laravelcd?utm_source=laravel.cd" target="_blank" class="text-gray-400 hover:text-gray-300">
                         <span class="sr-only">LinkedIn</span>
                         <x-icon.linkedin class="size-6" aria-hidden="true" />
                     </a>
 
-                    <a href="https://github.com/laravelcd?utm_source=laravel.cm" target="_blank" class="text-gray-400 hover:text-gray-300">
+                    <a href="https://github.com/laravelcd?utm_source=laravel.cd" target="_blank" class="text-gray-400 hover:text-gray-300">
                         <span class="sr-only">GitHub</span>
                         <x-icon.github class="size-6" aria-hidden="true" />
                     </a>
 
-                    <a href="https://www.youtube.com/channel/UCbQPQ8q31uQmuKtyRnATLSw?utm_source=laravel.cm" target="_blank" class="text-gray-400 hover:text-gray-300">
+                    <a href="https://www.youtube.com/@laravelcd?utm_source=laravel.cd" target="_blank" class="text-gray-400 hover:text-gray-300">
                         <span class="sr-only">YouTube</span>
                         <x-icon.youtube class="size-6" aria-hidden="true" />
                     </a>
